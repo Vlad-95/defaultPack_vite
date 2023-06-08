f95d4018285b5244ff8157bf728f0666117f166c
Vladislav <vzv@siluetstudio.com>
2023-06-08 10:10:54 +0300

    wip

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,6 @@ export default defineConfig({
     //   'https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Montserrat:wght@400;500;600;700&display=swap',
     // ]),
   ],
-  server: {
-    open: '/resources/index.html',
-  },
   base: './',
   server: {
     open: '/index.html',
